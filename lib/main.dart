4833209b66a699e3b05ddcb1e65ebc0b7c7cdf06
Ehsan <ehsanpc999@gmail.com>
2023-12-03 23:49:10 +0300

    Firebase setup is done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:wallet/feature/home/view/home_page.dart';
 import 'package:wallet/feature/home/view/send_balance.dart';
 import 'package:wallet/feature/register/signup.dart';
+import 'package:wallet/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -13,9 +19,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'BluePay',
-      home: SendBalancePage()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'BluePay',
+        home: HomePage());
   }
 }

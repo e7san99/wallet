f5411eeb85ae256c66db5c7cda1dd3e031275f22
Ehsan <ehsanpc999@gmail.com>
2024-01-11 14:50:21 +0300

    unfocus send & add balance

--- lib/feature/home/view/add_balance.dart
+++ lib/feature/home/view/add_balance.dart
@@ -16,107 +16,112 @@ class _AddBalancePageState extends State<AddBalancePage> {
   String? balance;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add Balance'),
-        centerTitle: true,
-        backgroundColor: backgroundColor,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocSelector<WalletCubit, WalletState, num?>(
-              selector: (state) {
-                return state.wallet?.balance;
-              },
-              builder: (context, balance) {
-                return RichText(
-                  text: TextSpan(
-                    text: balance?.toString() ??
-                        '0,00', //'${context.watch<WalletCubit>().state.wallet?.balance ?? 0}', //provider!.balance.toString()
-                    style: TextStyle(
-                      color: backgroundColor,
-                      fontSize: 43,
-                      fontWeight: FontWeight.w600,
-                      height: 0.9,
-                    ),
-                    children: <TextSpan>[
-                      TextSpan(
-                          text: ' IQD',
-                          style:
-                              TextStyle(fontSize: 20, color: backgroundColor)),
-                    ],
-                  ),
-                );
-              },
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            Text(
-              'Total Amount',
-              style: TextStyle(
-                color: backgroundColor.withOpacity(0.8),
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
-                height: 1,
-              ),
-            ),
-            Form(
-              key: formKey,
-              child: OwnTextFormField(
-                label: 'balance',
-                keyboardType: TextInputType.phone,
-                isNumber: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly,
-                ],
-                icon: Icons.attach_money,
-                onSaved: (value) {
-                  balance = value;
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Add Balance'),
+          centerTitle: true,
+          backgroundColor: backgroundColor,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              BlocSelector<WalletCubit, WalletState, num?>(
+                selector: (state) {
+                  return state.wallet?.balance;
                 },
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Enter a balance';
-                  }
-                  return null;
+                builder: (context, balance) {
+                  return RichText(
+                    text: TextSpan(
+                      text: balance?.toString() ??
+                          '0,00', //'${context.watch<WalletCubit>().state.wallet?.balance ?? 0}', //provider!.balance.toString()
+                      style: TextStyle(
+                        color: backgroundColor,
+                        fontSize: 43,
+                        fontWeight: FontWeight.w600,
+                        height: 0.9,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                            text: ' IQD',
+                            style: TextStyle(
+                                fontSize: 20, color: backgroundColor)),
+                      ],
+                    ),
+                  );
                 },
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            BlocSelector<WalletCubit, WalletState, bool>(
-              selector: (state) {
-                return state.isLoading;
-              },
-              builder: (context, state) {
-                return OwnButton(
-                  textButton: Text(
-                    'ADD',
-                    style: TextStyle(
-                      color: foregroundColor,
-                      fontSize: 20,
-                      fontFamily: 'lato',
-                      letterSpacing: 1.2,
+              const SizedBox(
+                height: 8,
+              ),
+              Text(
+                'Total Amount',
+                style: TextStyle(
+                  color: backgroundColor.withOpacity(0.8),
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                  height: 1,
+                ),
+              ),
+              Form(
+                key: formKey,
+                child: OwnTextFormField(
+                  label: 'balance',
+                  keyboardType: TextInputType.phone,
+                  isNumber: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly,
+                  ],
+                  icon: Icons.attach_money,
+                  onSaved: (value) {
+                    balance = value;
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Enter a balance';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              BlocSelector<WalletCubit, WalletState, bool>(
+                selector: (state) {
+                  return state.isLoading;
+                },
+                builder: (context, state) {
+                  return OwnButton(
+                    textButton: Text(
+                      'ADD',
+                      style: TextStyle(
+                        color: foregroundColor,
+                        fontSize: 20,
+                        fontFamily: 'lato',
+                        letterSpacing: 1.2,
+                      ),
                     ),
-                  ),
-                  onPressed: state
-                      ? null
-                      : () {
-                          if (!formKey.currentState!.validate()) return;
-                          formKey.currentState!.save();
-                          context
-                              .read<WalletCubit>()
-                              .updateBalance(num.parse(balance!));
-                          print("balance: ${num.parse(balance!)}");
-                        },
-                  backgroundColor: backgroundColor,
-                  foregroundColor: foregroundColor,
-                );
-              },
-            ),
-          ],
+                    onPressed: state
+                        ? null
+                        : () {
+                            if (!formKey.currentState!.validate()) return;
+                            formKey.currentState!.save();
+                            context
+                                .read<WalletCubit>()
+                                .updateBalance(num.parse(balance!));
+                            print("balance: ${num.parse(balance!)}");
+                          },
+                    backgroundColor: backgroundColor,
+                    foregroundColor: foregroundColor,
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/feature/home/view/send_balance.dart
+++ lib/feature/home/view/send_balance.dart
@@ -18,129 +18,134 @@ class _SendBalancePageState extends State<SendBalancePage> {
   Widget build(BuildContext context) {
     return Form(
       key: formKey,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Send Balance'),
-          centerTitle: true,
-          backgroundColor: backgroundColor,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              BlocSelector<WalletCubit, WalletState, num?>(
-                selector: (state) {
-                  return state.wallet?.balance;
-                },
-                builder: (context, balance) {
-                  return RichText(
-                    text: TextSpan(
-                      text: balance?.toString() ??
-                          '0,00', //provider!.balance.toString()
-                      style: TextStyle(
-                        color: backgroundColor,
-                        fontSize: 43,
-                        fontWeight: FontWeight.w600,
-                        height: 0.9,
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Send Balance'),
+            centerTitle: true,
+            backgroundColor: backgroundColor,
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                BlocSelector<WalletCubit, WalletState, num?>(
+                  selector: (state) {
+                    return state.wallet?.balance;
+                  },
+                  builder: (context, balance) {
+                    return RichText(
+                      text: TextSpan(
+                        text: balance?.toString() ??
+                            '0,00', //provider!.balance.toString()
+                        style: TextStyle(
+                          color: backgroundColor,
+                          fontSize: 43,
+                          fontWeight: FontWeight.w600,
+                          height: 0.9,
+                        ),
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: ' IQD',
+                              style: TextStyle(
+                                  fontSize: 20, color: backgroundColor)),
+                        ],
                       ),
-                      children: <TextSpan>[
-                        TextSpan(
-                            text: ' IQD',
-                            style: TextStyle(
-                                fontSize: 20, color: backgroundColor)),
-                      ],
-                    ),
-                  );
-                },
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              Text(
-                'Total Amount',
-                style: TextStyle(
-                  color: backgroundColor.withOpacity(0.8),
-                  fontSize: 20,
-                  fontWeight: FontWeight.w500,
-                  height: 1,
+                    );
+                  },
                 ),
-              ),
-              OwnTextFormField(
-                label: 'balance',
-                keyboardType: TextInputType.phone,
-                isNumber: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly,
-                ],
-                icon: Icons.attach_money,
-                onSaved: (value) {
-                  setState(() {
-                    balance = value;
-                  });
-                },
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Enter a balance';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              OwnTextFormField(
-                label: 'Phone',
-                keyboardType: TextInputType.phone,
-                isNumber: <TextInputFormatter>[phoneNumberFormatter],
-                icon: Icons.phone_outlined,
-                onSaved: (value) {
-                  setState(() {
-                    phone = value;
-                  });
-                },
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Enter a phone';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              BlocSelector<WalletCubit, WalletState, bool>(
-                selector: (state) {
-                  return state.isLoading;
-                },
-                builder: (context, isLoading) {
-                  return OwnButton(
-                    textButton: Text(
-                      'SEND',
-                      style: TextStyle(
-                        color: foregroundColor,
-                        fontSize: 20,
-                        fontFamily: 'lato',
-                        letterSpacing: 1.2,
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  'Total Amount',
+                  style: TextStyle(
+                    color: backgroundColor.withOpacity(0.8),
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                    height: 1,
+                  ),
+                ),
+                OwnTextFormField(
+                  label: 'balance',
+                  keyboardType: TextInputType.phone,
+                  isNumber: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly,
+                  ],
+                  icon: Icons.attach_money,
+                  onSaved: (value) {
+                    setState(() {
+                      balance = value;
+                    });
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Enter a balance';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                OwnTextFormField(
+                  label: 'Phone',
+                  keyboardType: TextInputType.phone,
+                  isNumber: <TextInputFormatter>[phoneNumberFormatter],
+                  icon: Icons.phone_outlined,
+                  onSaved: (value) {
+                    setState(() {
+                      phone = value;
+                    });
+                  },
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Enter a phone';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                BlocSelector<WalletCubit, WalletState, bool>(
+                  selector: (state) {
+                    return state.isLoading;
+                  },
+                  builder: (context, isLoading) {
+                    return OwnButton(
+                      textButton: Text(
+                        'SEND',
+                        style: TextStyle(
+                          color: foregroundColor,
+                          fontSize: 20,
+                          fontFamily: 'lato',
+                          letterSpacing: 1.2,
+                        ),
                       ),
-                    ),
-                    onPressed: isLoading
-                        ? null
-                        : () async {
-                            if (!formKey.currentState!.validate()) return;
-                            formKey.currentState!.save();
-                            context.read<WalletCubit>().sendBalance(
-                                  num.parse(balance!),
-                                  phone??'000',
-                                );
-                            print(
-                              "balance: ${num.parse(balance!)}",
-                            );
-                          },
-                    backgroundColor: backgroundColor,
-                    foregroundColor: foregroundColor,
-                  );
-                },
-              ),
-            ],
+                      onPressed: isLoading
+                          ? null
+                          : () async {
+                              if (!formKey.currentState!.validate()) return;
+                              formKey.currentState!.save();
+                              context.read<WalletCubit>().sendBalance(
+                                    num.parse(balance!),
+                                    phone ?? '000',
+                                  );
+                              print(
+                                "balance: ${num.parse(balance!)}",
+                              );
+                            },
+                      backgroundColor: backgroundColor,
+                      foregroundColor: foregroundColor,
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),

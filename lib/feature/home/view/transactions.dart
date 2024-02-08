64d235793f993df87493ebc30bfef4b401f05bc1
Ehsan <ehsanpc999@gmail.com>
2024-02-08 17:03:04 +0300

    animation list separate method

--- lib/feature/home/view/transactions.dart
+++ lib/feature/home/view/transactions.dart
@@ -109,32 +109,6 @@ class _TransactionsViewState extends State<TransactionsView> {
                     ),
                   );
                 } else {
-                  Matrix4 getTransformMatrix(TransformableListItem item) {
-                    /// final scale of child when the animation is completed
-                    const endScaleBound = 0.5;
-
-                    /// 0 when animation completed and [scale] == [endScaleBound]
-                    /// 1 when animation starts and [scale] == 1
-                    final animationProgress =
-                        item.visibleExtent / item.size.height;
-
-                    /// result matrix
-                    final paintTransform = Matrix4.identity();
-
-                    /// animate only if item is on edge
-                    if (item.position != TransformableListItemPosition.middle) {
-                      final scale = endScaleBound +
-                          ((1 - endScaleBound) * animationProgress);
-
-                      paintTransform
-                        ..translate(item.size.width / 2)
-                        ..scale(scale)
-                        ..translate(-item.size.width / 2);
-                    }
-
-                    return paintTransform;
-                  }
-
                   list.sort((a, b) => b.dateTime!.compareTo(a.dateTime!));
                   return Expanded(
                     child: TransformableListView.builder(
@@ -239,5 +213,29 @@ class _TransactionsViewState extends State<TransactionsView> {
         ),
       ),
     );
+  }
+
+  Matrix4 getTransformMatrix(TransformableListItem item) {
+    /// final scale of child when the animation is completed
+    const endScaleBound = 0.3;
+
+    /// 0 when animation completed and [scale] == [endScaleBound]
+    /// 1 when animation starts and [scale] == 1
+    final animationProgress = item.visibleExtent / item.size.height;
+
+    /// result matrix
+    final paintTransform = Matrix4.identity();
+
+    /// animate only if item is on edge
+    if (item.position != TransformableListItemPosition.middle) {
+      final scale = endScaleBound + ((1 - endScaleBound) * animationProgress);
+
+      paintTransform
+        ..translate(item.size.width / 2)
+        ..scale(scale)
+        ..translate(-item.size.width / 2);
+    }
+
+    return paintTransform;
   }
 }

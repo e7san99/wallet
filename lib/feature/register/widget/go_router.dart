c0aa5b9101b1b10078efbc4a6b7343a3fadcaeaf
Ehsan <ehsanpc999@gmail.com>
2024-02-12 14:35:30 +0300

    separate GoRouter page

--- lib/feature/register/widget/go_router.dart
+++ lib/feature/register/widget/go_router.dart
@@ -1,20 +1,45 @@
-// import 'package:go_router/go_router.dart';
-// /// The route configuration.
-// final GoRouter _router = GoRouter(
-//   routes: <RouteBase>[
-//     GoRoute(
-//       path: '/',
-//       builder: (BuildContext context, GoRouterState state) {
-//         return const HomeScreen();
-//       },
-//       routes: <RouteBase>[
-//         GoRoute(
-//           path: 'details',
-//           builder: (BuildContext context, GoRouterState state) {
-//             return const DetailsScreen();
-//           },
-//         ),
-//       ],
-//     ),
-//   ],
-// );
+    import 'package:go_router/go_router.dart';
+import 'package:wallet/feature/home/view/deposit.dart';
+import 'package:wallet/feature/home/view/send_balance.dart';
+import 'package:wallet/feature/home/view/transactions.dart';
+
+import '../view/view.dart';
+
+/// The route configuration.
+    final GoRouter router = GoRouter(
+      initialLocation: '/startPage',
+      routes: [
+        GoRoute(
+          path: '/startPage',
+          builder: (context, state) => const StartPage(),
+        ),
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const HomePage(),
+        ),
+        GoRoute(
+          path: '/signin',
+          builder: (context, state) => const SigninPage(),
+        ),
+        GoRoute(
+          path: '/signup',
+          builder: (context, state) => const SignupPage(),
+        ),
+        GoRoute(
+          path: '/forgot',
+          builder: (context, state) => const SignupPage(),
+        ),
+        GoRoute(
+          path: '/deposit',
+          builder: (context, state) => const DepositPage(),
+        ),
+        GoRoute(
+          path: '/sendBalance',
+          builder: (context, state) => const SendBalancePage(),
+        ),
+        GoRoute(
+          path: '/transaction',
+          builder: (context, state) => const TransactionsPage(),
+        ),
+      ],
+    );
--- lib/main/my_app.dart
+++ lib/main/my_app.dart
@@ -1,9 +1,7 @@
-import 'package:go_router/go_router.dart';
 import 'package:wallet/feature/home/cubit/bloc/internet_bloc.dart';
 import 'package:wallet/feature/home/cubit/wallet_cubit.dart';
-import 'package:wallet/feature/home/home.dart';
 import 'package:wallet/feature/home/repository/wallet/wallet_implement.dart';
-import 'package:wallet/feature/home/view/transactions.dart';
+import 'package:wallet/feature/register/widget/go_router.dart';
 import 'package:wallet/main/main_export.dart';
 
 class MyApp extends StatelessWidget {
@@ -11,44 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /// The route configuration.
-    final GoRouter router = GoRouter(
-      initialLocation: '/startPage',
-      routes: [
-        GoRoute(
-          path: '/startPage',
-          builder: (context, state) => const StartPage(),
-        ),
-        GoRoute(
-          path: '/',
-          builder: (context, state) => const HomePage(),
-        ),
-        GoRoute(
-          path: '/signin',
-          builder: (context, state) => const SigninPage(),
-        ),
-        GoRoute(
-          path: '/signup',
-          builder: (context, state) => const SignupPage(),
-        ),
-        GoRoute(
-          path: '/forgot',
-          builder: (context, state) => const SignupPage(),
-        ),
-        GoRoute(
-          path: '/deposit',
-          builder: (context, state) => const DepositPage(),
-        ),
-        GoRoute(
-          path: '/sendBalance',
-          builder: (context, state) => const SendBalancePage(),
-        ),
-        GoRoute(
-          path: '/transaction',
-          builder: (context, state) => const TransactionsPage(),
-        ),
-      ],
-    );
     return MultiBlocProvider(
       providers: [
         BlocProvider(
